15962a9942b5ce6e9db41df232b7c019d1669cc9
hcmutemimi <hoaimiqng@gmail.com>
2020-11-25 23:08:46 +0700

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
